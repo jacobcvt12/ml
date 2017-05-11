84a3a640abddc76c8980510f867f962b615a0256
Jacob Carey <JCarey@telligen.com>
2017-05-11 12:23:52 -0400

    Add column names to datasets

--- src/misc/read-in.R
+++ src/misc/read-in.R
@@ -17,5 +17,9 @@ file <- h5file(file.path(script.dir, "..", "..", "data",
 lshg <- file["data"][]
 h5close(file)
 
+colnames(lhg) <- c("y", paste0("X", 1:(ncol(lhg)-1)))
+colnames(lphg) <- c("y", paste0("X", 1:(ncol(lphg)-1)))
+colnames(lshg) <- c("y", paste0("X", 1:(ncol(lshg)-1)))
+
 set.seed(1)
 ind <- sample(2, nrow(lhg), TRUE, prob=c(0.7, 0.3))
